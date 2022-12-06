cf9e57e6250af3febf3f0b014f1181b392ca536c
naimur <naimur202163@gmail.com>
2022-12-06 21:21:26 +0600

    userSection

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const User =require('/models/User');
+
+// Registar
+
+router.post("/register",(req,res)=>{
+  const newUser=
+})
+
+
+
+module.exports=router;
