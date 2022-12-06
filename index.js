4a2e1637e963f19b945e6465a0825e7a423cd69d
naimur <naimur202163@gmail.com>
2022-12-06 21:00:31 +0600

    Env file added

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express=require("express");
-const app=express();
-const mongoose=require('mongoose');
-
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 // Database Connection
 
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log(`DB is connected successfully`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(4000,()=>{
+app.listen(process.env.PORT || 4000, () => {
   console.log(`The Server is running on port 4000`);
-})
+});
